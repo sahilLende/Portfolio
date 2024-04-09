8345408d7f10dc603e0d0bc54543c545f1d93d4f
Rogsahil10 <sahillende07@gmail.com>
2024-04-09 22:57:17 +0530

    Working update

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import "./globals.css";
 import Navigation from "./UI/Home/Navigation";
 import { eduQLD, inter, lexend } from "./font";
-import { UserProvider } from "@auth0/nextjs-auth0/client";
+
 import DM from "./UI/Components/ContactForm";
 import Footer from "./UI/Components/Footer";
 export const metadata = {
@@ -12,18 +12,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <UserProvider>
-        <body
-          className={`${inter.variable} ${eduQLD.variable} ${lexend.variable}   min-w-[250px]  2xl:mx-auto   2xl:w-[80%]   5xl:w-1/2 `}
-        >
-          <Navigation />
-          <main className="body-text1  w-full  pt-16 2xl:border-x-2   ">
-            {children}
-          </main>
-          <DM />
-          <Footer />
-        </body>
-      </UserProvider>
+      <body
+        className={`${inter.variable} ${eduQLD.variable} ${lexend.variable}   min-w-[250px]  2xl:mx-auto   2xl:w-[80%]   5xl:w-1/2 `}
+      >
+        <Navigation />
+        <main className="body-text1  w-full  pt-16 2xl:border-x-2   ">
+          {children}
+        </main>
+        <DM />
+        <Footer />
+      </body>
     </html>
   );
 }
--- app/visitorsbook/page.js
+++ app/visitorsbook/page.js
@@ -1,5 +1,4 @@
 import { FingerPrintIcon } from "@heroicons/react/24/outline";
-import { UserProvider } from "@auth0/nextjs-auth0/client";
 const page = () => {
   return <div className="m-4 min-h-[50vh] ">Adding Soon!</div>;
 };
