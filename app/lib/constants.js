0696635b9f8b26b8ba5c84415392d3b59e343416
sahillende <sahillende07@gmail.com>
2024-05-08 00:29:07 +0530

    updated projects

--- app/lib/constants.js
+++ app/lib/constants.js
@@ -2,45 +2,53 @@ const projects = [
   {
     title: "Dash Dish", // Project Title
     description:
-      " Dish Dash, a food ordering app, using React (front-end) and Express/MongoDB (backend). Streamlined menu exploration with intuitive filters (query params) for a delightful user experience.",
-    tags: ["JavaScript", "ReactJS", "Express", "MongoDB", "Tailwind Css"], // Array of project tags
+      " Dish Dash, a food ordering app, using React (Client side) and Express/MongoDB (server side). Streamlined menu exploration with pagination and intuitive filters (query params) for a delightful user experience.",
+    tags: [
+      "JavaScript",
+      "ReactJS",
+      "Express",
+      "MongoDB",
+      "Tailwind Css",
+      "Python",
+    ], // Array of project tags
     hrefs: {
       live: "https://dashdish.netlify.app", // Link to project demo
-      readme: "linktoNewpagein website", // Link to GitHub repository
+      readme: "https://sahillende.vercel.app", // Link to GitHub repository
       repo: "github.com/sahilLende/Client-Dash-Dish", // Link to project website
     },
   },
 
   {
-    title: "Portfoilio", // Project Title
+    title: "Portfolio", // Project Title
     description:
       "Next.js & Postgres fuel my portfolio, featuring live comments & NextAuth security. Drop a line in my guestbook - I'm all ears!",
     tags: ["JavaScript", "NextJS", "PostGresSQL", "0Auth", "Tailwind Css"], // Array of project tags
     hrefs: {
-      live: "dashdish.netlify.app", // Link to project demo
-      readme: "linktoNewpagein website", // Link to GitHub repository
-      repo: "github.com/sahilLende/Client-Dash-Dish", // Link to project website
+      live: "https://sahillende.vercel.app", // Link to project demo
+      readme: "https://sahillende.vercel.app", // Link to GitHub repository
+      repo: "github.com/sahilLende/", // Link to project website
     },
   },
   {
-    title: "Portfoiliso", // Project Title
+    title: "ReactJs Cloth Shop", // Project Title
     description:
-      "Explore my portfolio powS ensures a responsive design. PostgreSQL for feature like guestbook.",
-    tags: ["JavaScript", "NextJS", "PostGresSQL", "0Auth", "Tailwind Css"], // Array of project tags
+      "A SPA made using ReactJS.It leverages Firebase for secure and efficient data management and user authentication. The application supports features like shopping cart management, and secure checkout processes",
+    tags: ["ReactJs", "Firebase"], // Array of project tags
     hrefs: {
-      live: "dashdish.netlify.app", // Link to project demo
-      readme: "linktoNewpagein website", // Link to GitHub repository
-      repo: "github.com/sahilLende/Client-Dash-Dish", // Link to project website
+      live: "https://sahillende.vercel.app", // Link to project demo
+      readme: "https://sahillende.vercel.app", // Link to GitHub repository
+      repo: "github.com/sahilLende", // Link to project website
     },
   },
   {
-    title: "KM Real Estate", // Project Title
-    description: "Exploreonsive design. PostgreSQL for feature like guestbook.",
-    tags: ["JavaScript", "NextJS", "PostGresSQL", "0Auth", "Tailwind Css"], // Array of project tags
+    title: "CURL Clone", // Project Title
+    description:
+      "Simple CURL clone made using JS.Used sockets for  HTTP requests",
+    tags: ["JavaScript", "Html", "Css", "sockets"], // Array of project tags
     hrefs: {
-      live: "dashdish.netlify.app", // Link to project demo
-      readme: "linktoNewpagein website", // Link to GitHub repository
-      repo: "github.com/sahilLende/Client-Dash-Dish", // Link to project website
+      live: "https://sahillende.vercel.app", // Link to project demo
+      readme: "https://sahillende.vercel.app", // Link to GitHub repository
+      repo: "github.com/sahilLende", // Link to project website
     },
   } /* 4Lwr4WjqPNd8kvT */,
 
